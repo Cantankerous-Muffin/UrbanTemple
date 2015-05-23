52a33a5ff2ccf2d9b4cfde7aa2d1d8b21a360833
Prakal <pranav.89@gmail.com>
2015-05-23 16:10:30 -0700

    add documentation folder, remove unnecessary angular related files, update schema

--- app/config.js
+++ app/config.js
@@ -12,46 +12,48 @@ db.plugin('registry');
 /**
  * Columns email, firstName, lastName, shippingAddress and phoneNumber are currently not being used.
  */
-db.knex.schema.hasTable('users').then(function(exists) {
+db.knex.schema.hasTable('students').then(function(exists) {
   if (!exists) {
-    db.knex.schema.createTable('users', function (user) {
-      user.increments('id').primary();
-      user.string('username', 100).unique();
-      user.string('password', 100);
-      user.string('email', 100);
-      user.string('venmoUsername', 255);
-      user.string('venmoDisplayName', 255);
-      user.string('venmoUserId', 255);
-      user.string('venmoAccessToken', 255);
-      user.string('venmoRefreshToken', 255);
-      user.string('venmoPicture', 255);
-      user.string('firstName', 100);
-      user.string('lastName', 100);
-      user.string('shippingAddress', 255);
-      user.string('phoneNumber', 100);
-      user.timestamps();
+    db.knex.schema.createTable('students', function (student) {
+      student.increments('id').primary();
+      student.string('username', 100).unique();
+      student.string('password', 100);
+      student.string('email', 100);
+      student.string('firstName', 100);
+      student.string('lastName', 100);
+      student.timestamps();
     }).then(function (table) {
       console.log('Created Table', table);
     });
   }
 });
 
-db.knex.schema.hasTable('events').then(function(exists) {
+db.knex.schema.hasTable('instructors').then(function(exists) {
   if (!exists) {
-    db.knex.schema.createTable('events', function (event) {
-      event.increments('id').primary();
-      event.string('title', 255);
-      event.text('description');
-      event.string('image',255);
-      event.dateTime('expiration');
-      event.string('user_id', 100);
-      event.integer('invites');
-      event.integer('thresholdPeople');
-      event.integer('thresholdMoney');
-      event.integer('committedPeople');
-      event.integer('committedMoney');
-      event.boolean('paid');
-      event.timestamps();
+    db.knex.schema.createTable('instructors', function (instructor) {
+      instructor.increments('id').primary();
+      instructor.string('username', 100).unique();
+      instructor.string('password', 100);
+      instructor.string('email', 100);
+      instructor.string('firstName', 100);
+      instructor.string('lastName', 100);
+      instructor.timestamps();
+    }).then(function (table) {
+      console.log('Created Table', table);
+    });
+  }
+});
+
+db.knex.schema.hasTable('classes').then(function(exists) {
+  if (!exists) {
+    db.knex.schema.createTable('classes', function (class) {
+      class.increments('id').primary();
+      class.string('title', 255);
+      class.string('video_URL', 255);
+      class.string('instructor_id', 10);
+      class.text('description');
+      class.string('image',255);
+      class.timestamps();
     }).then(function (table) {
       console.log('Created Table', table);
     });
