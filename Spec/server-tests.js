11c38e8a2945de2bcb4dd4c9a6a3e224a66b2dfd
Prakal <pranav.89@gmail.com>
2015-05-25 19:27:48 -0700

    develop pattern for running tests on DB in HTML

--- Spec/server-tests.js
+++ Spec/server-tests.js
@@ -1,14 +1,20 @@
+var expect = require('chai').expect;
 var db = require('../app/config');
 var Students = require('../app/models/student');
+console.log('students',Students);
 
-console.log('Gets to tests');
+// Can run DB queries inside HTTP requests for information in the DB.
+beforeEach(function() {
+	new Students({
+		username: 'jim'
+	}).fetch().then(function(a){console.log('a')});
+});
+
 describe("DB tests", function() {
   var a;
 
   it("put a value in the database and query it", function() {
-  	console.log('This gets called');
-
     a = true;
-    expect(a).toBe(true);
+    expect(a).to.equal(true);
   });
 });
