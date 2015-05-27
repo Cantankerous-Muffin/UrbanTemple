6338ea1b2c38503e585668a6db4757f72ebebcb8
Ken <khwken@gmail.com>
2015-05-27 16:16:54 -0700

    setup controller for auth

--- public/assets/js/apps/Auth/auth_app.js
+++ public/assets/js/apps/Auth/auth_app.js
@@ -1,24 +1,18 @@
 VirtualDojo.module("AuthApp", function(AuthApp, VirtualDojo, Backbone, Marionette, $, _){
   AuthApp.Router = Marionette.AppRouter.extend({
     appRoutes: {
-      "login": "authenticate",
+      "login": "renderLoginPage",
     }
   });
-
-  var API = {
-    authenticate: function(){
-      AuthApp.Login.Controller.showLogin();
-    },
-  };
  
   VirtualDojo.on("authenticate:login", function(){
-	API.authenticate();
-	VirtualDojo.navigate("login");
+		this.controller.renderLoginPage();
+		VirtualDojo.navigate("login");
   });
 
   AuthApp.on("start", function(){
     new AuthApp.Router({
-      controller: API
+	    controller: new AuthApp.Controller({})
     });
   });
 });
--- public/assets/js/apps/Auth/login/login_controller.js
+++ public/assets/js/apps/Auth/login/login_controller.js
@@ -6,3 +6,6 @@ VirtualDojo.module("AuthApp.Login", function(Login, VirtualDojo, Backbone, Mario
     }
   };
 });
+
+
+
