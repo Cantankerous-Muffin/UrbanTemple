b89f5c80412622fd59fc94b0c4485b3ca22fdd75
TheWhiteOx <thewhiteoxinbox@gmail.com>
2015-05-28 17:34:16 -0700

    Add skeleton controller for Sidenav subapp

---
+++ public/js/apps/Sidenav/sidenav_controller.js
@@ -0,0 +1,18 @@
+define([
+    "app",
+    "apps/Sidenav/sidenav/sidenav_view"
+  ],
+    function(VirtualDojo, SidenavView) {
+
+      VirtualDojo.module("SidenavApp", function(SidenavApp, VirtualDojo, Backbone, Marionette, $, _){
+        SidenavApp.Controller = {
+
+          showSidenav: function() {
+            VirtualDojo.regions.sidenav.show(new SidenavView.view());
+          }
+        }
+      });
+
+      return VirtualDojo.SidenavApp.Controller;
+    }
+  )
