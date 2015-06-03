2be1e8692cadd8015b885474bba469143e4c9113
TheWhiteOx <thewhiteoxinbox@gmail.com>
2015-06-02 19:05:27 -0700

    Add skeleton controller code for Level subapp

---
+++ public/js/apps/Level/level_controller.js
@@ -0,0 +1,15 @@
+define([
+    "app",
+    "entities/models/level_models",
+    "apps/Level/video/level_view"
+  ], 
+  function(VirtualDojo, LevelModels, LevelView){
+    VirtualDojo.module("LevelApp", function(LevelApp, VirtualDojo, Backbone, Marionette, $, _){
+
+      LevelApp.Controller = {
+        
+      }
+
+    });
+    return VirtualDojo.LevelApp.Controller
+  });
