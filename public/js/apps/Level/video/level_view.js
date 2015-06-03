58f18daf6897397230a901548cf0f197d1d99e73
TheWhiteOx <thewhiteoxinbox@gmail.com>
2015-06-02 19:10:15 -0700

    Add skeleton code for Level subapp view

---
+++ public/js/apps/Level/video/level_view.js
@@ -0,0 +1,13 @@
+define([
+    "app",
+    "tpl!apps/Level/video/templates/level_view.tpl"
+  ], 
+  function(VirtualDojo,LevelViewTpl){
+
+    VirtualDojo.module("LevelApp.View", function(View, VirtualDojo, Backbone, Marionette, $, _){
+
+      ////
+
+    })
+    return VirtualDojo.LevelApp.View;
+  })
