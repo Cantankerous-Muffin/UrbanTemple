a5d359a3693c859360755d6aaecc11f19fbc2c71
Prakal <pranav.89@gmail.com>
2015-06-09 18:00:50 -0700

    modify route for /user/:username/feedbacks to account for authentication

--- routes/user.js
+++ routes/user.js
@@ -189,6 +189,59 @@ router.get('/:username/progress', function(req,res){
 });
 
 router.get('/:username/feedbacks', function(req, res) {
+	console.log('req.url is', req.url, req.session.user);
+	var markers = [];
+	for (var i = 1; i < req.url.length; i++){
+		if (req.url[i] === '\/'){
+			markers.push(i);
+		}
+	}
+	console.log('markers',markers,req.url.slice(1,markers[0]));
+	var usernameFromURL = req.url.slice(1,markers[0]);
+	db.knex('students')
+		.where({'students.username': usernameFromURL})
+		.select('id')
+		.then(function(user_id){
+			if (!user_id[0]){
+				console.log('perhaps an instructor?')
+				db.knex('instructors')
+					.where({'instructors.username': usernameFromURL})
+					.then(function(instructorData){
+						console.log('username',instructorData[0].username);
+						if (req.session.user === instructorData[0].username){
+							db.knex('feedback')
+								.where({'feedback.instructor_id': instructorData[0].id})
+								.then(function(feedbackData){
+									console.log('feedbackData', feedbackData);
+									res.json(feedbackData);
+								});
+						} else {
+							res.json({'message': 'incorrect user signed in to view instructor feedbacks'});
+						}
+					})
+					.catch(function(err){
+						res.json({'message': 'No student/instructor with this username found.'});
+					});
+			} else {
+				if (user_id[0].id === req.session.user){
+					db.knex('feedback')
+						.where({'feedback.student_id': user_id[0].id})
+						.then(function(feedbackData){
+							console.log('feedbackData', feedbackData);
+							res.json(feedbackData);
+						});
+				} else {
+					res.json({'message': 'incorrect user signed in to view students'});
+				}
+			}
+		})
+		// .catch(function(err){
+		// 	res.json({'message': 'No student with this username found.'});
+		// });
+});
+
+
+router.get('/:username/feedbacks/:feedback_id', function(req, res) {
 	console.log('req.url is', req.url);
 	var markers = [];
 	for (var i = 1; i < req.url.length; i++){
@@ -198,6 +251,8 @@ router.get('/:username/feedbacks', function(req, res) {
 	}
 	console.log('markers',markers,req.url.slice(1,markers[0]));
 	var usernameFromURL = req.url.slice(1,markers[0]);
+	var feedbackIDFromURL = req.url.slice(markers[1]+1);
+	console.log(usernameFromURL,feedbackIDFromURL);
 	db.knex('students')
 		.where({'students.username': usernameFromURL})
 		.select('id')
@@ -230,7 +285,5 @@ router.get('/:username/feedbacks', function(req, res) {
 		// 	res.json({'message': 'No student with this username found.'});
 		// });
 });
-
-
 
 module.exports = router;
