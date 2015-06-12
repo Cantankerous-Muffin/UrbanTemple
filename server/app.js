9e49cad19860b4bb4b3f920836f9bff00bc284b4
Prakal <pranav.89@gmail.com>
2015-06-12 15:02:14 -0700

    serve doc (jsdocs) statically

--- server/app.js
+++ server/app.js
@@ -26,6 +26,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, '../public')));
+app.use(express.static(path.join(__dirname, '../doc')));
 
 // Initiate passport and passport session
 app.use(passport.initialize());
